743999d6eb9fbac89137571a469d7088fbbb7b71
Sergei <qaserge@gmail.com>
2023-09-19 21:22:42 -0400

    Implemented Petition update() and destroy()

--- app/Http/Controllers/PetitionController.php
+++ app/Http/Controllers/PetitionController.php
@@ -6,6 +6,7 @@ use App\Http\Resources\PetitionCollection;
 use App\Http\Resources\PetitionResource;
 use App\Models\Petition;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class PetitionController extends Controller
 {
@@ -14,7 +15,9 @@ class PetitionController extends Controller
      */
     public function index()
     {
-        return new PetitionCollection(Petition::all()); // if return PetitionCollection::collection(Petition::all()); it'll return only data
+        // return new PetitionCollection(Petition::all()); 
+        // if return PetitionCollection::collection(Petition::all()); it'll return only data
+        return response()->json(new PetitionCollection(Petition::all()), Response::HTTP_OK);
     }
 
     /**
@@ -42,7 +45,13 @@ class PetitionController extends Controller
      */
     public function update(Request $request, Petition $petition)
     {
-        //
+        // @TODO add auth check
+
+        $petition->update($request->only([
+            'title', 'description', 'category', 'author', 'signees'
+        ]));
+
+        return new PetitionResource($petition);
     }
 
     /**
@@ -50,6 +59,10 @@ class PetitionController extends Controller
      */
     public function destroy(Petition $petition)
     {
-        //
+        // @TODO add auth check
+
+        $petition->delete();
+
+        return response()->json(null, Response::HTTP_NO_CONTENT);
     }
 }
